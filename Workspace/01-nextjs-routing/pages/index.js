18d32d19435eb3d88672af193fc613d39679b902
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-08 18:55:28 +0530

    Navigating with the "Link" Component

--- Workspace/01-nextjs-routing/pages/index.js
+++ Workspace/01-nextjs-routing/pages/index.js
@@ -9,6 +9,10 @@ function HomePage() {
       <h1>The Home Page</h1>
       <ul>
         <li>
+          {/* This special Link component renders an anchor tag but it watches clicks on those anchor tags
+                and if you click there, it prevents the browser default of sending a request and getting a new HTML page.
+                Instead, it will load the to be loaded component for you and change the URL
+                so that it looks like you changed the page but in reality, you stay in that single page application.*/}
           <Link href='/portfolio'>Portfolio</Link>
         </li>
         <li>
