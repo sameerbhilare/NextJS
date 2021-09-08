e256f09b9137452d6a23b3e901b3a594f0a207e0
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-08 18:50:54 +0530

    Adding Catch-All Routes

--- Workspace/01-nextjs-routing/pages/blog/[...slug].js
+++ Workspace/01-nextjs-routing/pages/blog/[...slug].js
@@ -1,8 +1,25 @@
-import { useRouter } from "next/router";
+/*
+  This is Catch-All route.
+  The spread operator (...) tells NextJS to treat this file in a special way (i.e. to catch all)
+  and redirects all requests within ourdomain.com/blog/* to this function.
+  e.g. 
+    ourdomain.com/blog, 
+    ourdomain.com/blog/some-blog-id,
+    ourdomain.com/blog/2021/01
+    etc.
+*/
+import { useRouter } from 'next/router';
 
 function BlogPostsPage() {
   const router = useRouter();
 
+  /*
+    Here 'router.query' returns 'slug' property (name of this file) with
+    value as array of string for different segments caught by it.
+    e.g. 
+      For ourdomain.com/blog/2021, => router.query = {slug: ["2021"]}
+      For ourdomain.com/blog/2021/01, => router.query = {slug: ["2021","01"]}
+  */
   console.log(router.query);
 
   return (
