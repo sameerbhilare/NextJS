739fe4c8690d436fc7157b21740316025960ba45
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-08 19:12:04 +0530

    Adding Custom 404 page

--- Workspace/01-nextjs-routing/pages/404.js
+++ Workspace/01-nextjs-routing/pages/404.js
@@ -1,3 +1,7 @@
+/*
+  The name of this file must be '404.js' to catch all 404 (Page Not Found) errors. )
+  Next.js will then always load this component when a 404 error occurs.
+*/
 function NotFoundPage() {
   return (
     <div>
