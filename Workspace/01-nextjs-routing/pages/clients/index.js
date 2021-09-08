79efc84b9aef7a9c184a5e16934a9260b115c0e4
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-08 19:02:38 +0530

    Navigating To Dynamic Routes

--- Workspace/01-nextjs-routing/pages/clients/index.js
+++ Workspace/01-nextjs-routing/pages/clients/index.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 
 function ClientsPage() {
+  // assume this clients list is fetched from backend (so dynamic array)
   const clients = [
     { id: 'max', name: 'Maximilian' },
     { id: 'manu', name: 'Manuel' },
@@ -12,6 +13,11 @@ function ClientsPage() {
       <ul>
         {clients.map((client) => (
           <li key={client.id}>
+            {/* Dynamic Routes via Link -
+                This special Link component renders an anchor tag but it watches clicks on those anchor tags
+                and if you click there, it prevents the browser default of sending a request and getting a new HTML page.
+                Instead, it will load the to be loaded component for you and change the URL
+                so that it looks like you changed the page but in reality, you stay in that single page application.*/}
             <Link
               href={{
                 pathname: '/clients/[id]',
