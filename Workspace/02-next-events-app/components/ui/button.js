ddb1ec79bfb764081a7a7289ad4f3cc1f2ff2e9c
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-09 19:00:29 +0530

    Adding a Filter Form for Filtering Events

--- Workspace/02-next-events-app/components/ui/button.js
+++ Workspace/02-next-events-app/components/ui/button.js
@@ -3,13 +3,21 @@ import Link from 'next/link';
 import classes from './button.module.css';
 
 const Button = (props) => {
+  if (props.link) {
+    return (
+      <Link href={props.link}>
+        {/* Link tag in the end renders anchor tag only.
+              However if we want to apply custom styling, then we need to add that anchor tag on our own.
+              Rest of the functionality will be added by the Link component automcatically */}
+        <a className={classes.btn}>{props.children}</a>
+      </Link>
+    );
+  }
+
   return (
-    <Link href={props.link}>
-      {/* Link tag in the end renders anchor tag only.
-        However if we want to apply custom styling, then we need to add that anchor tag on our own.
-        Rest of the functionality will be added by the Link component automcatically */}
-      <a className={classes.btn}>{props.children}</a>
-    </Link>
+    <button ame={classes.btn} onClick={props.onClick}>
+      {props.children}
+    </button>
   );
 };
 
