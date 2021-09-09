02c1ebc39c7f0801d017da4d556bbe0d501898cd
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-09 19:08:33 +0530

    Navigating to the "Filtered Events" Page Progammatically

--- Workspace/02-next-events-app/pages/events/index.js
+++ Workspace/02-next-events-app/pages/events/index.js
@@ -1,13 +1,20 @@
 import { getAllEvents } from '../../dummy-data';
 import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
+import { useRouter } from 'next/router';
 
 const AllEventsPage = () => {
   const events = getAllEvents();
+  const router = useRouter();
+
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  };
 
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </>
   );
