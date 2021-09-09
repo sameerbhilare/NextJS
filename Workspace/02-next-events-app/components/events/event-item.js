d354f1d41c978c18c985561327b9fcefc81ce594
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-09 17:54:19 +0530

    Adding More React Components & Connecting Components

--- Workspace/02-next-events-app/components/events/event-item.js
+++ Workspace/02-next-events-app/components/events/event-item.js
@@ -1,5 +1,35 @@
+import Link from 'next/link';
+
 const EventItem = (props) => {
-  return <li></li>;
+  const { title, image, date, location, id } = props;
+
+  const readableDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+
+  const formattedAddress = location.replace(', ', '\n');
+
+  return (
+    <li>
+      <img src={'/' + image} alt={title} />
+      <div>
+        <div>
+          <h2>{title}</h2>
+          <div>
+            <time>{readableDate}</time>
+          </div>
+          <div>
+            <address>{formattedAddress}</address>
+          </div>
+        </div>
+        <div>
+          <Link href={`/events/${id}`}>Explore Event</Link>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default EventItem;
