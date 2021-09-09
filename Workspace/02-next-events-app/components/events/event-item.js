77454bd0276a18bfd2effc312ced6701a0cc4240
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-09 18:16:46 +0530

    Adding Buttons & Icons

--- Workspace/02-next-events-app/components/events/event-item.js
+++ Workspace/02-next-events-app/components/events/event-item.js
@@ -1,4 +1,7 @@
-import Link from 'next/link';
+import Button from '../ui/button';
+import DateIcon from '../icons/date-icon';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
 
 import classes from './event-item.module.css';
 
@@ -20,14 +23,21 @@ const EventItem = (props) => {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{readableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={`/events/${id}`}>Explore Event</Link>
+          <Button link={`/events/${id}`}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
