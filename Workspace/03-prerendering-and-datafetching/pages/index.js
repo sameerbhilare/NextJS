2521ffefac51be6c0dfdab98ab84c4c51e0b4e6e
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-20 13:52:47 +0530

    Running Server-side Code & Using the Filesystem

--- Workspace/03-prerendering-and-datafetching/pages/index.js
+++ Workspace/03-prerendering-and-datafetching/pages/index.js
@@ -1,11 +1,35 @@
+import path from 'path'; // from nodejs (backend)
+import fs from 'fs'; // from nodejs (backend)
+
 function HomePage(props) {
+  const { products } = props;
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((p) => (
+        <li key={p.id}>{p.title}</li>
+      ))}
     </ul>
   );
 }
 
 export default HomePage;
+
+/*
+  getStaticProps prepares the 'props' for this (HomePage) component.
+  The code inside getStaticProps will never be visible on the client side.
+
+  NextJs wil first call this getStaticProps function and then executes the component (HomePage) function. 
+  Both these steps are run at build time
+*/
+export async function getStaticProps(context) {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = fs.readFileSync(filePath);
+  const data = JSON.parse(jsonData);
+
+  // must return object with 'props' key
+  return {
+    props: {
+      products: data.products,
+    },
+  };
+}
