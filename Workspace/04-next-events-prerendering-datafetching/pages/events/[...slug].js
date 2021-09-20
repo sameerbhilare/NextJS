c4ef2d2d65b40a8ba68fb013062465975df5ac3a
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-20 18:35:30 +0530

    Using Server-side Rendering (SSR)

--- Workspace/04-next-events-prerendering-datafetching/pages/events/[...slug].js
+++ Workspace/04-next-events-prerendering-datafetching/pages/events/[...slug].js
@@ -1,21 +1,73 @@
 import { useRouter } from 'next/router';
-import { getFilteredEvents } from '../../dummy-data';
+import { getFilteredEvents } from '../../helpers/api-util';
 import EventList from '../../components/events/event-list';
 import ResultsTitle from '../../components/events/results-title';
 import Button from '../../components/ui/button';
 import ErrorAlert from '../../components/ui/error-alert';
 
-const FilteredEventsPage = () => {
+const FilteredEventsPage = (props) => {
   const router = useRouter();
 
-  const filterData = router.query.slug;
+  // const filterData = router.query.slug;
 
-  if (!filterData) {
-    // this component is loaded twice, first - as a call to this route and second time because of useRouter
-    // for first load, the useRouter does not have required data (router.query.slug)
-    return <p className='center'>Loading...</p>;
+  // if (!filterData) {
+  //   // this component is loaded twice, first - as a call to this route and second time because of useRouter
+  //   // for first load, the useRouter does not have required data (router.query.slug)
+  //   return <p className='center'>Loading...</p>;
+  // }
+
+  // const filteredYear = filterData[0];
+  // const filteredMonth = filterData[1];
+
+  // // convert to number
+  // const numYear = +filteredYear;
+  // const numMonth = +filteredMonth;
+
+  if (props.hasError) {
+    return (
+      <>
+        <ErrorAlert>
+          <p>Invalid Filter. Please adjust your values!</p>
+        </ErrorAlert>
+        <div className='center'>
+          <Button link='/events'>Show All Events</Button>
+        </div>
+      </>
+    );
   }
 
+  const filteredEvents = props.events;
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <>
+        <ErrorAlert>
+          <p>No events found for the chosen filter!</p>
+        </ErrorAlert>
+        <div className='center'>
+          <Button link='/events'>Show All Events</Button>
+        </div>
+      </>
+    );
+  }
+
+  const date = new Date(props.date.year, props.date.month - 1);
+
+  return (
+    <>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </>
+  );
+};
+
+export default FilteredEventsPage;
+
+// server side rendering
+export async function getServerSideProps(context) {
+  const { params } = context;
+
+  const filterData = params.slug;
   const filteredYear = filterData[0];
   const filteredMonth = filterData[1];
 
@@ -31,41 +83,29 @@ const FilteredEventsPage = () => {
     numMonth < 1 ||
     numMonth > 12
   ) {
-    return (
-      <>
-        <ErrorAlert>
-          <p>Invalid Filter. Please adjust your values!</p>
-        </ErrorAlert>
-        <div className='center'>
-          <Button link='/events'>Show All Events</Button>
-        </div>
-      </>
-    );
+    return {
+      // custom handling
+      props: {
+        hasError: true,
+      },
+
+      // noFound: true, // show 404 page for invalid
+      // redirect: {
+      //   destination: '/error'
+      // }
+    };
   }
 
-  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
+  const filteredEvents = await getFilteredEvents({ year: numYear, month: numMonth });
 
-  if (!filteredEvents || filteredEvents.length === 0) {
-    return (
-      <>
-        <ErrorAlert>
-          <p>No events found for the chosen filter!</p>
-        </ErrorAlert>
-        <div className='center'>
-          <Button link='/events'>Show All Events</Button>
-        </div>
-      </>
-    );
-  }
-
-  const date = new Date(numYear, numMonth - 1);
-
-  return (
-    <>
-      <ResultsTitle date={date} />
-      <EventList items={filteredEvents} />
-    </>
-  );
-};
-
-export default FilteredEventsPage;
+  // return filered events
+  return {
+    props: {
+      events: filteredEvents,
+      date: {
+        year: numYear,
+        month: numMonth,
+      },
+    },
+  };
+}
