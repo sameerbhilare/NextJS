22beffa885cfecf0a3dca4a900d9111e39b539f9
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-20 18:22:58 +0530

    Working on the "All Events" Page

--- Workspace/04-next-events-prerendering-datafetching/pages/events/index.js
+++ Workspace/04-next-events-prerendering-datafetching/pages/events/index.js
@@ -1,10 +1,10 @@
-import { getAllEvents } from '../../dummy-data';
+import { getAllEvents } from '../../helpers/api-util';
 import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
 import { useRouter } from 'next/router';
 
-const AllEventsPage = () => {
-  const events = getAllEvents();
+const AllEventsPage = (props) => {
+  const events = props.events;
   const router = useRouter();
 
   const findEventsHandler = (year, month) => {
@@ -21,3 +21,17 @@ const AllEventsPage = () => {
 };
 
 export default AllEventsPage;
+
+// static site rendering
+export async function getStaticProps(context) {
+  // all events
+  const allEvents = await getAllEvents();
+
+  // return all events
+  return {
+    props: {
+      events: allEvents,
+    },
+    revalidate: 60, // every 60 secs we regenereate this page for new incoming request
+  };
+}
