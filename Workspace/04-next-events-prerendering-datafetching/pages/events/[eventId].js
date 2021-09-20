1609297c328dfbee414ee3e84b3cf5ba63fed76d
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-20 18:12:04 +0530

    Loading Data & Paths For Dynamic Pages

--- Workspace/04-next-events-prerendering-datafetching/pages/events/[eventId].js
+++ Workspace/04-next-events-prerendering-datafetching/pages/events/[eventId].js
@@ -1,15 +1,11 @@
-import { useRouter } from 'next/router';
-import { getEventById } from '../../dummy-data';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
 import ErrorAlert from '../../components/ui/error-alert';
+import { getEventById, getAllEvents } from '../../helpers/api-util';
 
-const EventDetailPage = () => {
-  const router = useRouter();
-
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+const EventDetailPage = (props) => {
+  const event = props.selectedEvent;
 
   if (!event) {
     return (
@@ -36,3 +32,28 @@ const EventDetailPage = () => {
 };
 
 export default EventDetailPage;
+
+// for which eventIds it should pre-render this page
+export async function getStaticPaths() {
+  const allEvents = await getAllEvents();
+  const paramEvents = allEvents.map((event) => ({ params: { eventId: event.id } }));
+
+  return {
+    paths: paramEvents,
+    fallback: false, // bcz we did specify all possible paths so for unknown events, nextjs will show 404
+  };
+}
+
+// page pre-rendering
+export async function getStaticProps(context) {
+  // get access to params
+  const { params } = context;
+
+  const event = await getEventById(params.eventId);
+
+  return {
+    props: {
+      selectedEvent: event,
+    },
+  };
+}
