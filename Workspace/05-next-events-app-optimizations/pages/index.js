ddfd00244b2eaa89a977acd2c3132732cde25cd9
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-20 19:05:13 +0530

    Configuring the "head" Content

--- Workspace/05-next-events-app-optimizations/pages/index.js
+++ Workspace/05-next-events-app-optimizations/pages/index.js
@@ -1,9 +1,16 @@
+import Head from 'next/head';
 import EventList from '../components/events/event-list';
 import { getFeaturedEvents } from '../helpers/api-util';
 
 const HomePage = (props) => {
   return (
     <div>
+      {/*  Next.js injects the content which we add between these 'Head' tags 
+      into the real 'head' part of the rendered page. */}
+      <Head>
+        <title>NextJS Events</title>
+        <meta name='description' content='Find a lot of great events that allow you to evolve...' />
+      </Head>
       <EventList items={props.events} />
     </div>
   );
