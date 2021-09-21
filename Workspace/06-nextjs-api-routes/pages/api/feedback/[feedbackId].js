2001ea19fdf4308a8a2f6ded911412d1a0635fa2
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-21 18:15:01 +0530

    Exploring Different Ways Of Structuring API Route Files

--- Workspace/06-nextjs-api-routes/pages/api/feedback/[feedbackId].js
+++ Workspace/06-nextjs-api-routes/pages/api/feedback/[feedbackId].js
@@ -1,4 +1,4 @@
-import { buildFeedbackPath, extractFeedback } from '../api/feedback';
+import { buildFeedbackPath, extractFeedback } from '.';
 
 function handler(req, res) {
   const feedbackId = req.query.feedbackId; // query and path params
