6dada6161caca66e7552b36d638080b0031bf820
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-21 15:55:17 +0530

    Optimizing Images with the "Next Image" Component & Feature

--- Workspace/05-next-events-app-optimizations/components/event-detail/event-logistics.js
+++ Workspace/05-next-events-app-optimizations/components/event-detail/event-logistics.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import AddressIcon from '../icons/address-icon';
 import DateIcon from '../icons/date-icon';
 import LogisticsItem from './logistics-item';
@@ -16,7 +17,27 @@ function EventLogistics(props) {
   return (
     <section className={classes.logistics}>
       <div className={classes.image}>
-        <img src={`/${image}`} alt={imageAlt} />
+        {/* Next.js will create multiple versions of our image on the fly when requests are coming in,
+        then optimize those images for the operating systems and device sizes that are making the request.
+        And then those generated images will be cached for future requests from similar devices.
+        
+        We need to set width and height to basically inform Next.js 
+        about the width and height which we need for this image so that Nextjs will generate them accordingly.
+        width => 400px bcz 160 px is too blurry 10rem = 160px (10 rem defined in event-logistics.module.css )
+        height => 400px bcz 160 px is too blurry 10rem = 160px (10 rem defined in event-logistics.module.css)
+        
+        Based on this nextjs will generate optimized images, 
+        you can see those generated optimized images at .next/cache/images folder.
+        
+        They're not generated in advance, but only when a request reaches the page.
+        But then they are stored/cached so that future requests from a similar devices
+        immediately get that already generated image.
+        
+        Another nice feature offerred by this nextjs Image component is,
+        not all images on a webpage are downloaded
+        but only images which are visible on the current viewport are downloaded.
+        So we don't load what we don't need. :)  */}
+        <Image src={`/${image}`} alt={imageAlt} width={400} height={400} />
       </div>
       <ul className={classes.list}>
         <LogisticsItem icon={DateIcon}>
--- Workspace/05-next-events-app-optimizations/components/events/event-item.js
+++ Workspace/05-next-events-app-optimizations/components/events/event-item.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Button from '../ui/button';
 import DateIcon from '../icons/date-icon';
 import AddressIcon from '../icons/address-icon';
@@ -18,7 +19,28 @@ const EventItem = (props) => {
 
   return (
     <li className={classes.item}>
-      <img src={'/' + image} alt={title} />
+      {/* Next.js will create multiple versions of our image on the fly when requests are coming in,
+      then optimize those images for the operating systems and device sizes that are making the request.
+      And then those generated images will be cached for future requests from similar devices.
+      
+      We need to set width and height to basically inform Next.js 
+      about the width and height which we need for this image.
+      width => 40% of 640px. (40rem = 640 px, 40 rem defined in event-list.module.css )
+      height => 10rem = 160px (10 rem defined in event-item.module.css)
+      
+      Based on this nextjs will generate optimized images, 
+      you can see those generated optimized images at .next/cache/images folder.
+      
+      They're not generated in advance, but only when a request reaches the page.
+      But then they are stored/cached so that future requests from a similar devices
+      immediately get that already generated image.
+      
+      Another nice feature offerred by this nextjs Image component is,
+      not all images on a webpage are downloaded
+      but only images which are visible on the current viewport are downloaded.
+      So we don't load what we don't need. :)  */}
+      <Image src={'/' + image} alt={title} width={250} height={160} />
+
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
