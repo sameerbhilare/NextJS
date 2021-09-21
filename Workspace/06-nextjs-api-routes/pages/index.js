af94b0c08759b927149fba4c53e31492e20085ef
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-21 16:25:08 +0530

    Preparing the Frontend Form

--- Workspace/06-nextjs-api-routes/pages/index.js
+++ Workspace/06-nextjs-api-routes/pages/index.js
@@ -1,7 +1,30 @@
+import { useRef } from 'react';
+
 function HomePage() {
+  const emailRef = useRef();
+  const feedbackRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailRef.current.value;
+    const enteredFeedback = feedbackRef.current.value;
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form>
+        <div>
+          <label htmlFor='email'>Your Email Address</label>
+          <input type='email' id='email' ref={emailRef} />
+        </div>
+        <div>
+          <label htmlFor='feedback'>Your Feedback</label>
+          <textarea id='feedback' rows='5' ref={feedbackRef} />
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 }
