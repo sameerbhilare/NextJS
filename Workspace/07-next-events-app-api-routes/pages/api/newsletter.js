fd69f4cd5a7651f42bc73e5ecd899a6cf4a3c79d
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-22 17:01:23 +0530

    Running MongoDB Queries From Inside API Routes

--- Workspace/07-next-events-app-api-routes/pages/api/newsletter.js
+++ Workspace/07-next-events-app-api-routes/pages/api/newsletter.js
@@ -2,10 +2,11 @@
     Request to ourdomain.com/api/newsletter will reach here.
     Any code we write in here, will never end up in any client side code bundle.
 */
+import { MongoClient } from 'mongodb';
 
 // The default exported function(handler) will receive http request and response objects
 // inside this function we can write any server side code.
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
 
@@ -16,6 +17,19 @@ function handler(req, res) {
     }
 
     console.log({ userEmail });
+    // connect to mongodb
+    const client = await MongoClient.connect('mongodb://localhost:27017/nextjs-events');
+    const db = client.db();
+
+    // select collection in which you want to insert document
+    const meetupCollections = db.collection('emails');
+    // insert document in db
+    const result = await meetupCollections.insertOne({ email: userEmail });
+    console.log(result);
+
+    // close connection
+    client.close();
+
     res.status(201).json({ message: 'Signed up!' });
   }
 }
