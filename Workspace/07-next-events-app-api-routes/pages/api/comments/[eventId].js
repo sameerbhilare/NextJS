3c0e9765ba6b60fe3ee4fa07d3d26d829a546ad2
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-22 18:42:59 +0530

    Inserting Comments Into The Database

--- Workspace/07-next-events-app-api-routes/pages/api/comments/[eventId].js
+++ Workspace/07-next-events-app-api-routes/pages/api/comments/[eventId].js
@@ -1,11 +1,15 @@
 /* 
     Any code we write in here, will never end up in any client side code bundle.
 */
+import { MongoClient } from 'mongodb';
 
 // The default exported function(handler) will receive http request and response objects
 // inside this function we can write any server side code.
-function handler(req, res) {
+async function handler(req, res) {
   const eventId = req.query.eventId;
+
+  // connect to mongodb
+  const client = await MongoClient.connect('mongodb://localhost:27017/nextjs-events');
 
   if (req.method === 'POST') {
     const { email, name, text } = req.body;
@@ -23,8 +27,18 @@ function handler(req, res) {
       return;
     }
 
-    const newComment = { id: new Date().toISOString(), email, name, text };
+    const newComment = { email, name, text, eventId };
     console.log(newComment);
+
+    const db = client.db();
+
+    // select collection in which you want to insert document
+    const commentsCollections = db.collection('comments');
+
+    // insert document in db
+    const result = await commentsCollections.insertOne(newComment);
+    newComment.id = result.insertedId;
+
     res.status(201).json({ message: 'Added comment', comment: newComment });
     return;
   }
@@ -37,6 +51,9 @@ function handler(req, res) {
     res.status(200).json({ comments: dummyList });
     return;
   }
+
+  // close connection
+  client.close();
 }
 
 export default handler;
