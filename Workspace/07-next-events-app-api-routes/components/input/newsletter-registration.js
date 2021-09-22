b05e91e97dc89d3912eceddac98e9c2ad31326f4
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-22 16:27:16 +0530

    Adding a Newsletter Route

--- Workspace/07-next-events-app-api-routes/components/input/newsletter-registration.js
+++ Workspace/07-next-events-app-api-routes/components/input/newsletter-registration.js
@@ -1,12 +1,24 @@
+import { useRef } from 'react';
 import classes from './newsletter-registration.module.css';
 
 function NewsletterRegistration() {
+  const emailRef = useRef();
+
   function registrationHandler(event) {
     event.preventDefault();
 
-    // fetch user input (state or refs)
-    // optional: validate input
+    const enteredEmail = emailRef.current.value;
+
     // send valid data to API
+    fetch('/api/newsletter', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email: enteredEmail }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   }
 
   return (
@@ -14,7 +26,13 @@ function NewsletterRegistration() {
       <h2>Sign up to stay updated!</h2>
       <form onSubmit={registrationHandler}>
         <div className={classes.control}>
-          <input type='email' id='email' placeholder='Your email' aria-label='Your email' />
+          <input
+            type='email'
+            id='email'
+            ref={emailRef}
+            placeholder='Your email'
+            aria-label='Your email'
+          />
           <button>Register</button>
         </div>
       </form>
---
+++ Workspace/07-next-events-app-api-routes/pages/api/newsletter.js
@@ -0,0 +1,23 @@
+/* 
+    Request to ourdomain.com/api/feedback will reach here.
+    Any code we write in here, will never end up in any client side code bundle.
+*/
+
+// The default exported function(handler) will receive http request and response objects
+// inside this function we can write any server side code.
+function handler(req, res) {
+  if (req.method === 'POST') {
+    const userEmail = req.body.email;
+
+    // some validation
+    if (!userEmail || !userEmail.includes('@')) {
+      res.status(422).json({ message: 'Invalid Email Address!' });
+      return;
+    }
+
+    console.log({ userEmail });
+    res.status(201).json({ message: 'Signed up!' });
+  }
+}
+
+export default handler;
