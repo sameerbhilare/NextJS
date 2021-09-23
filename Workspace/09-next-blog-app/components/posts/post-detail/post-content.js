54833d6ccd657c28c5b77fca844e7239bfb19338
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-23 19:40:11 +0530

    Rendering Code Snippets From Markdown

--- Workspace/09-next-blog-app/components/posts/post-detail/post-content.js
+++ Workspace/09-next-blog-app/components/posts/post-detail/post-content.js
@@ -1,5 +1,7 @@
 import ReactMarkdown from 'react-markdown';
 import Image from 'next/image';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
@@ -49,6 +51,13 @@ function PostContent(props) {
       }
 
       return <p>{paragraph.children}</p>;
+    },
+
+    // for rendering code markdown snippets
+    code(code) {
+      const { className, children } = code;
+      const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
+      return <SyntaxHighlighter style={atomDark} language={language} children={children} />;
     },
   };
 
