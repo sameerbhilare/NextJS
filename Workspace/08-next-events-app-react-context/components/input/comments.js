38a3ec4d4cb7493de317cd11072bf4857d445f08
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-23 09:55:17 +0530

    Show/Hide notification when comment added

--- Workspace/08-next-events-app-react-context/components/input/comments.js
+++ Workspace/08-next-events-app-react-context/components/input/comments.js
@@ -1,11 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CommentList from './comment-list';
 import NewComment from './new-comment';
 import classes from './comments.module.css';
+import NotificationContext from '../../store/notification-context';
 
 function Comments(props) {
   const { eventId } = props;
+
+  const notificationCtx = useContext(NotificationContext);
 
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState([]);
@@ -24,6 +27,12 @@ function Comments(props) {
   }
 
   function addCommentHandler(commentData) {
+    notificationCtx.showNotification({
+      title: 'Sending comment',
+      message: 'Your comment is currently being stored to database',
+      status: 'pending',
+    });
+
     // send data to API
     fetch('/api/comments/' + eventId, {
       method: 'POST',
@@ -32,8 +41,30 @@ function Comments(props) {
       },
       body: JSON.stringify(commentData),
     })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+
+        // error case
+        return response.json().then((data) => {
+          throw new Error(data.message || 'Something went wrong!');
+        });
+      })
+      .then((data) => {
+        notificationCtx.showNotification({
+          title: 'Success!',
+          message: 'Your comment was saved!',
+          status: 'success',
+        });
+      })
+      .catch((error) => {
+        notificationCtx.showNotification({
+          title: 'Error',
+          message: error.message || 'Something went wrong!',
+          status: 'error',
+        });
+      });
   }
 
   return (
