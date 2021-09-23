0b805b7e07cb38e28633252cb3451af9a3d2ef51
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-23 09:34:13 +0530

    Adding Context State

--- Workspace/08-next-events-app-react-context/store/notification-context.js
+++ Workspace/08-next-events-app-react-context/store/notification-context.js
@@ -9,6 +9,13 @@ const NotificationContext = createContext({
 });
 
 export function NotificationContextProvider(props) {
+  // context state
+  /*
+  Whenever we change this state here, for example, by calling show or hideNotificationHandler in the end,
+  this NotificationContext component will re-render and will distribute the updated context object 
+  to interested components.
+  */
+
   const [activeNotification, setActiveNotification] = useState();
 
   useEffect(() => {
