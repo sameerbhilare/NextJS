b7b780d4fe19eb6db3df288540bb90e67ce53a40
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-23 09:57:46 +0530

    show loader while fetching comments

--- Workspace/08-next-events-app-react-context/components/input/comments.js
+++ Workspace/08-next-events-app-react-context/components/input/comments.js
@@ -12,13 +12,18 @@ function Comments(props) {
 
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState([]);
+  const [isFetchingComments, setIsFetchingComments] = useState(false);
 
   useEffect(() => {
     if (showComments) {
+      setIsFetchingComments(true);
       // send data to API
       fetch('/api/comments/' + eventId)
         .then((response) => response.json())
-        .then((data) => setComments(data.comments));
+        .then((data) => {
+          setComments(data.comments);
+          setIsFetchingComments(false);
+        });
     }
   }, [showComments]);
 
@@ -71,7 +76,8 @@ function Comments(props) {
     <section className={classes.comments}>
       <button onClick={toggleCommentsHandler}>{showComments ? 'Hide' : 'Show'} Comments</button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList items={comments} />}
+      {showComments && !isFetchingComments && <CommentList items={comments} />}
+      {showComments && isFetchingComments && <p>Loading...</p>}
     </section>
   );
 }
