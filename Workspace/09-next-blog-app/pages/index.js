9e1a00003a8b55c1adc6fe8aea0a6a152bd4874b
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-23 14:30:25 +0530

    Getting Started With The Home Page

--- Workspace/09-next-blog-app/pages/index.js
+++ Workspace/09-next-blog-app/pages/index.js
@@ -1,3 +1,14 @@
-function HomePage() {}
+function HomePage() {
+  /*
+    1. Hero
+    2. Featured Posts
+  */
+  return (
+    <>
+      <Hero />
+      <FeaturedPosts />
+    </>
+  );
+}
 
 export default HomePage;
