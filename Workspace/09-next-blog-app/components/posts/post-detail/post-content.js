5b727f2b4be680aff5d55218dc88f81849f33824
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-23 19:24:33 +0530

    Rendering Images With The "Next Image" Component (From Markdown)

--- Workspace/09-next-blog-app/components/posts/post-detail/post-content.js
+++ Workspace/09-next-blog-app/components/posts/post-detail/post-content.js
@@ -1,15 +1,61 @@
 import ReactMarkdown from 'react-markdown';
+import Image from 'next/image';
+
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
 
 function PostContent(props) {
   const { post } = props;
+
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    /* 
+    react-markdown will call this method if it finds a 'image' in the Markdown content.
+    So for this snippet of .md
+        ![Create routes via your file + folder structure](nextjs-file-based-routing.png)
+    it will populate the image input to below image() function such that - 
+        image.src = 'nextjs-file-based-routing.png'
+        image.alt = 'Create routes via your file + folder structure'
+    */
+    // img(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+
+      // instead of rendering a paragraph with the image inside of it,
+      // we should instead want to render just an image without that paragraph as it is not valid html content
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+  };
 
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
