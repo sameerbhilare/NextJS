2d5eac8494fb87f4835697d71f1093c087e0426f
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-23 15:43:57 +0530

    Adding A Post Grid & Post Items

--- Workspace/09-next-blog-app/components/posts/post-item.js
+++ Workspace/09-next-blog-app/components/posts/post-item.js
@@ -1,9 +1,36 @@
+import Link from 'mext/link';
+import Image from 'mext/image';
 import classes from './post-item.module.css';
 
 function PostItem(props) {
-  const { post } = props;
+  const { title, image, excerpt, date, slug } = props.post;
 
-  return <li></li>;
+  // formatted date
+  const formattedDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+
+  // prepare image path
+  const imagePath = `/images/posts/${slug}/${image}`;
+
+  return (
+    <li className={classes.post}>
+      <Link>
+        <a>
+          <div className={classes.image}>
+            <Image src={imagePath} alt={title} width={300} height={200} />
+          </div>
+          <div className={classes.content}>
+            <h3>{title}</h3>
+            <time>{formattedDate}</time>
+            <p>{excerpt}</p>
+          </div>
+        </a>
+      </Link>
+    </li>
+  );
 }
 
 export default PostItem;
