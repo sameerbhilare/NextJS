0c7a663f6ab4dfe1376b8ea7e978908abec006f1
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-23 09:44:33 +0530

    Triggering & Showing Notifications

--- Workspace/08-next-events-app-react-context/components/input/newsletter-registration.js
+++ Workspace/08-next-events-app-react-context/components/input/newsletter-registration.js
@@ -1,13 +1,23 @@
-import { useRef } from 'react';
+import { useContext, useRef } from 'react';
+import NotificationContext from '../../store/notification-context';
 import classes from './newsletter-registration.module.css';
 
 function NewsletterRegistration() {
   const emailRef = useRef();
 
+  // use context
+  const notificationCtx = useContext(NotificationContext);
+
   function registrationHandler(event) {
     event.preventDefault();
 
     const enteredEmail = emailRef.current.value;
+
+    notificationCtx.showNotification({
+      title: 'Signing up',
+      message: 'Registering for Newsletter',
+      status: 'pending',
+    });
 
     // send valid data to API
     fetch('/api/newsletter', {
@@ -17,8 +27,30 @@ function NewsletterRegistration() {
       },
       body: JSON.stringify({ email: enteredEmail }),
     })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+
+        // error case
+        return response.json().then((data) => {
+          throw new Error(data.message || 'Something went wrong!');
+        });
+      })
+      .then((data) => {
+        notificationCtx.showNotification({
+          title: 'Signing up',
+          message: 'Successfully registered for Newsletter!',
+          status: 'success',
+        });
+      })
+      .catch((error) => {
+        notificationCtx.showNotification({
+          title: 'Error',
+          message: error.message || 'Something went wrong!',
+          status: 'error',
+        });
+      });
   }
 
   return (
