e789d225cf2882eaaaff030384e6e55805a1d08c
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-24 10:05:01 +0530

    Sending Data From The Client To The API Route

--- Workspace/09-next-blog-app/components/contact/contact-form.js
+++ Workspace/09-next-blog-app/components/contact/contact-form.js
@@ -1,23 +1,47 @@
+import { useRef } from 'react';
 import classes from './contact-form.module.css';
 
 function ContactForm() {
+  const emailRef = useRef();
+  const nameRef = useRef();
+  const messageRef = useRef();
+
+  function sendMessageHandler(event) {
+    event.preventDefault();
+
+    const message = {
+      email: emailRef.current.value,
+      name: nameRef.current.value,
+      message: messageRef.current.value,
+    };
+
+    // send message
+    fetch('/api/contact', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(message),
+    });
+  }
+
   return (
     <section className={classes.contact}>
       <h1>How may I help you?</h1>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor='email'>Your Email</label>
-            <input type='email' id='email' required />
+            <input type='email' id='email' required ref={emailRef} />
           </div>
           <div className={classes.control}>
             <label htmlFor='name'>Your Name</label>
-            <input type='text' id='name' required />
+            <input type='text' id='name' required ref={nameRef} />
           </div>
         </div>
         <div className={classes.control}>
           <label htmlFor='message'>Your Message</label>
-          <textarea id='message' rows='5' />
+          <textarea id='message' rows='5' ref={messageRef} />
         </div>
 
         <div className={classes.actions}>
