e34006c5d474b2364577822209e7a06119b791f4
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-09-27 17:37:02 +0530

    Deploying to production

--- Workspace/11-nextjs-auth/next.config.js
+++ Workspace/11-nextjs-auth/next.config.js
@@ -15,6 +15,9 @@ module.exports = (phase) => {
     env: {
       mongodb_connection_string:
         'mongodb+srv://<USERNAME>:<PASSWORD>@cluster0.zdoxp.mongodb.net/<DB_NAME>?retryWrites=true&w=majority',
+
+      // for prod, we must set 'NEXTAUTH_URL' url to your domain name so that next-auth will take care of few things behnd the scenes
+      NEXTAUTH_URL: 'https://nextjs-blog-b386wno1j-sameerbhilare.vercel.app',
     },
   };
 };
